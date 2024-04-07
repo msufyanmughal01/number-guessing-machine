91fdb37b0cb61ba5eb84b4d87bf2eeb7d66edcc2
M Sufyan Mughal <mskhalid703@gmail.com>
2024-04-07 11:47:01 -0700

    cli based guessed number

---
+++ index.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+const randomnumber = 18;
+const answer = await inquirer.prompt([{
+        name: "userguessednumber",
+        type: "number",
+        message: "please gueseed a number"
+    }]);
+if (answer.userguessednumber === randomnumber) {
+    console.log("congratulation! you guessed the right answer");
+}
+else {
+    console.log("you guessed the wrong answer");
+}
